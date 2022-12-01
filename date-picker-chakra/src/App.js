29de91b4dc629af0981c4d93a4c6497290781d08
amatureApe <amatureApe@gmail.com>
2022-12-01 10:46:12 -0800

    add chakra date-picker

---
+++ date-picker-chakra/src/App.js
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import DatePicker from './components';
+
+function App() {
+  return (
+    <Box>
+      <DatePicker />
+    </Box>
+  );
+}
+
+export default App;
---
+++ date-picker-chakra/src/components/index.js
@@ -0,0 +1,11 @@
+import { Box, Stack, Input, Text, Heading } from '@chakra-ui/react'
+
+const DatePicker = () => {
+    return (
+        <Box>
+            <Text>Hello World</Text>
+        </Box>
+    )
+}
+
+export default DatePicker
